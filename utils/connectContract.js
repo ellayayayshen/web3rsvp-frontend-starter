a9343a81c2ea1404464925b33493a5213e605879
Ella Shen <ellayshen@gmail.com>
2022-08-17 23:42:51 +0100

    final

---
+++ utils/connectContract.js
@@ -0,0 +1,25 @@
+import abiJSON from "./Web3RSVP.json";
+import { ethers } from "ethers";
+
+function connectContract() {
+    const contractAddress = "0xbE54c25e9Ae22d8Cb7d240c188109EF68B10190f";
+    const contractABI = abiJSON.abi;
+    let rsvpContract;
+    try {
+      const { ethereum } = window;
+  
+      if (ethereum) {
+        //checking for eth object in the window
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        rsvpContract = new ethers.Contract(contractAddress, contractABI, signer); // instantiating new connection to the contract
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
+    return rsvpContract;
+  }
+  
+  export default connectContract;
